53db64cf4b75d15acd762e1ff640b366d7863fd5
TranquilWind <ghouse.exe@gmail.com>
2025-08-30 06:41:13 +0530

    reduced app size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,9 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+        ndk {
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a")
+        }
     }
 
     buildTypes {
